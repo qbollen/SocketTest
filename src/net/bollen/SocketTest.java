3232428d14f44d16d1ff9fa682dbd328f4d954fa
bollen <qbollen@gmail.com>
2013-11-05 15:12:11 +0800

    finished window open

--- src/net/bollen/SocketTest.java
+++ src/net/bollen/SocketTest.java
@@ -1,6 +1,28 @@
 package net.bollen;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class SocketTest
 {
-
+	private JFrame frame;
+	
+	public SocketTest()
+	{
+		frame = new JFrame("SocketTest");
+		frame.setBounds(100,100,800,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				new SocketTest();
+			}
+		});
+	}
 }
